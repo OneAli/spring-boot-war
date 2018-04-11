5bf5ffc767f4d12a810724fa7da91fc7decc6c8e
admin <admin@localhost>
2018-04-11 15:34:44 +0800

    init

---
+++ src/main/java/com/my/Application.java
@@ -0,0 +1,29 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class Application extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String greeting() {
+		return "Spring boot war application!";
+	}
+
+}
